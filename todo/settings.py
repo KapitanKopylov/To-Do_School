3dec27681dc41545b8f7564a3431bcc4dac0c328
KapitanKopylov <79024434405@mail.ru>
2024-01-11 21:32:04 +0500

    Changed some settings

--- todo/settings.py
+++ todo/settings.py
@@ -35,8 +35,6 @@ for i in range(10):
 
 # Application definition
 
-# DEFAULT_CHARSET = 'utf-8'
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "TheBestToDoListEver/To-Do_School/todoapp/static")
 

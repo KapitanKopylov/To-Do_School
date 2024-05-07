96da7d8a471da6cbd76a4993ac192efd6932b210
KapitanKopylov <79024434405@mail.ru>
2024-05-07 23:57:06 +0500

    Clean allowed_hosts

--- todo/settings.py
+++ todo/settings.py
@@ -24,13 +24,7 @@ SECRET_KEY = 'django-insecure-1ca^yl9v7h#e%7dn=pt$+wnld^rq*s=_2y8+u!^o^l8$=v5c__
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1",
-                 "192.168.63.160",
-                 "192.168.34.160",
-                 "192.168.153.160",
-                 "192.168.139.160",
-                 '192.168.254.160',
-                 "TheBestToDoListEver.pythonanywhere.com",
+ALLOWED_HOSTS = ["TheBestToDoListEver.pythonanywhere.com",
                  "www.TheBestToDoListEver.pythonanywhere.com"]
 for i in range(256):
     ALLOWED_HOSTS.append(f"192.168.0.{i}")
